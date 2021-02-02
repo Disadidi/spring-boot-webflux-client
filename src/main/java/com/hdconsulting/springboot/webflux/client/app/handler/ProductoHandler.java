29eaf7017edebb5f39d1dadd6ce1381020b09bb4
Hugo Disadidi <hdisadidiconsulting@gmail.com>
2021-02-02 14:06:47 +0100

    factorisation error 404 not found

--- src/main/java/com/hdconsulting/springboot/webflux/client/app/handler/ProductoHandler.java
+++ src/main/java/com/hdconsulting/springboot/webflux/client/app/handler/ProductoHandler.java
@@ -36,23 +36,13 @@ public class ProductoHandler {
 
 		String id = request.pathVariable("id");
 
-		return service.findById(id)
+		return errorHandler(
+				service.findById(id)
 				.flatMap(p -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
 				//.syncBody(p))
 						.body(BodyInserters.fromValue(p)))
 				.switchIfEmpty(ServerResponse.notFound().build())
-				.onErrorResume(error -> {
-					WebClientResponseException errorResponse = (WebClientResponseException) error;
-					
-					if (errorResponse.getStatusCode() == HttpStatus.NOT_FOUND) {
-						Map<String, Object> body = new HashMap<>();
-						body.put("error", "No existe el producto: ".concat(errorResponse.getMessage()));
-						body.put("timestamp", new Date());
-						body.put("status", errorResponse.getStatusCode().value());
-						return ServerResponse.status(HttpStatus.NOT_FOUND).body(BodyInserters.fromValue(body));
-					}
-					return Mono.error(errorResponse);
-				});
+				);
 
 	}
 
@@ -88,57 +78,54 @@ public class ProductoHandler {
 		Mono<Producto> producto = request.bodyToMono(Producto.class);
 		String id = request.pathVariable("id");
 		
-		return producto
+		return errorHandler(
+				producto
 				.flatMap(p -> service.update(p, id))
 				.flatMap(p -> ServerResponse.created(URI.create("/api/client/".concat(id)))
 				.contentType(MediaType.APPLICATION_JSON)
 				//.body(p)
 				.body(BodyInserters.fromValue(p)))
-				.onErrorResume(error -> {
-					WebClientResponseException errorResponse = (WebClientResponseException) error;
-					
-					if (errorResponse.getStatusCode() == HttpStatus.NOT_FOUND) {
-						return ServerResponse.notFound().build();
-					}
-					return Mono.error(errorResponse);
-				});
+				);
 	}
 	
 	public Mono<ServerResponse> eliminar(ServerRequest request) {
 		String id = request.pathVariable("id");
 		
-		return service.delete(id)
+		return errorHandler(
+				service.delete(id)
 				.then(ServerResponse.noContent().build())
-				.onErrorResume(error -> {
-					WebClientResponseException errorResponse = (WebClientResponseException) error;
-					
-					if (errorResponse.getStatusCode() == HttpStatus.NOT_FOUND) {
-						return ServerResponse.notFound().build();
-					}
-					return Mono.error(errorResponse);
-				});
+				);
 		
 	}
 	
 	public Mono<ServerResponse> upload(ServerRequest request){
 		String id = request.pathVariable("id");
 		
-		return request.multipartData().map(multipart -> multipart.toSingleValueMap().get("file"))
+		return errorHandler(
+				request.multipartData().map(multipart -> multipart.toSingleValueMap().get("file"))
 				.cast(FilePart.class)
 				.flatMap(file -> service.upload(file, id))
 				.flatMap(p -> ServerResponse.created(URI.create("/api/client/".concat(p.getId())))
 				.contentType(MediaType.APPLICATION_JSON)
 				//.syncBody(p))
 				.body(BodyInserters.fromValue(p)))
-				.onErrorResume(error -> {
-					WebClientResponseException errorResponse = (WebClientResponseException) error;
-					
-					if (errorResponse.getStatusCode() == HttpStatus.NOT_FOUND) {
-						return ServerResponse.notFound().build();
-					}
-					return Mono.error(errorResponse);
-				});
+				);
 				
+	}
+	
+	private Mono<ServerResponse> errorHandler(Mono<ServerResponse> response){
+		return response.onErrorResume(error -> {
+			WebClientResponseException errorResponse = (WebClientResponseException) error;
+			
+			if (errorResponse.getStatusCode() == HttpStatus.NOT_FOUND) {
+				Map<String, Object> body = new HashMap<>();
+				body.put("error", "No existe el producto: ".concat(errorResponse.getMessage()));
+				body.put("timestamp", new Date());
+				body.put("status", errorResponse.getStatusCode().value());
+				return ServerResponse.status(HttpStatus.NOT_FOUND).body(BodyInserters.fromValue(body));
+			}
+			return Mono.error(errorResponse);
+		});
 	}
 
 }
